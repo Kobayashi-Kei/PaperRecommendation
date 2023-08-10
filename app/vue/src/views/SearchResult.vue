dfb09a0f1bcbe31cb78289ffea543cc0b7342cd2
Kobayashi-Kei <ketakeketake@gmail.com>
2023-08-11 00:28:01 +0900

    文言を少し変更

--- app/vue/src/views/SearchResult.vue
+++ app/vue/src/views/SearchResult.vue
@@ -124,7 +124,7 @@ const downloadJSON = () => {
 <template>
     <SearchForm v-bind:inputText="inputText"/>
     <div class="mt-3 mb-3">
-        <p>各論文の <span class="text-blue-500">Read more</span> をクリックすると，全文と観点別の下線を表示します．&nbsp;
+        <p>各論文の <span class="text-blue-500">Read more</span> をクリックすると，アブストラクト全文と観点別の下線を表示します．&nbsp;
                 <label class="relative inline-flex items-center cursor-pointer">
                     <input type="checkbox" v-model="isHighlightLabel" class="sr-only peer">
                     <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
